a69e82b8afbd041d04910cc8d7d189898a0e39df
Subham1999 <subhamsantra2016@gmail.com>
2021-04-16 20:14:53 +0530

    Udpate on crud-controller -by Subham Santra

--- src/com/subham/dsnalgo/array/rearrangements/ArrayDebugUtil.java
+++ src/com/subham/dsnalgo/array/rearrangements/ArrayDebugUtil.java
@@ -10,28 +10,28 @@ import java.util.Arrays;
  *
  */
 public final class ArrayDebugUtil {
-    public void print(int[] a) {
-	System.out.println(Arrays.toString(a));
-    }
-
-    public void swap(int[] a, int from, int to) {
-	if (0 <= from && from < a.length) {
-	    if (0 <= to && to < a.length) {
-		int tmp = a[from];
-		a[from] = a[to];
-		a[to] = tmp;
-	    }
+	public void print(int[] a) {
+		System.out.println(Arrays.toString(a));
 	}
-    }
 
-    public int partition(int[] a, final int pivot) {
-	int j = 0;
-	for (int i = 0; i < a.length; ++i) {
-	    if (a[i] > pivot) {
-		swap(a, j, i);
-		++j;
-	    }
+	public void swap(int[] a, int from, int to) {
+		if (0 <= from && from < a.length) {
+			if (0 <= to && to < a.length) {
+				int tmp = a[from];
+				a[from] = a[to];
+				a[to] = tmp;
+			}
+		}
 	}
-	return j;
-    }
+
+	public int partition(int[] a, final int pivot) {
+		int j = 0;
+		for (int i = 0; i < a.length; ++i) {
+			if (a[i] > pivot) {
+				swap(a, j, i);
+				++j;
+			}
+		}
+		return j;
+	}
 }
--- src/com/subham/test/Fruit.java
+++
@@ -1,8 +0,0 @@
-package com.subham.test;
-
-@SuppressWarnings("preview")
-public sealed abstract class Fruit permits Mango {
-	public String getTaste() {
-		return "wow";
-	}
-}
